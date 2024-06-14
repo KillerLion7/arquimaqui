fdc2f659fd482b9ba0824c301cf08e5fa8747993
Javier Gaitán <52298237+KillerLion7@users.noreply.github.com>
2024-06-14 12:23:17 -0600

    Pagina disque terminada

---
+++ PaginaWeb/js/script.js
@@ -0,0 +1,10 @@
+function toggleMenu(id) {
+    var menu = document.getElementById(id);
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+
